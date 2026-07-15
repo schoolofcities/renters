64501fb0c6754231fdb39c11efd650e79db0e391
an-k45 <an_k27@protonmail.com>
2026-07-15 13:02:25 -0400

    build

--- docs/_app/immutable/chunks/B79aEfnt.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_ff9zmi?.base??"/renters",e=globalThis.__sveltekit_ff9zmi?.assets??s;export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/mawqOACX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_uc3yu7?.base??"/renters",e=globalThis.__sveltekit_uc3yu7?.assets??s;export{e as a,s as b};
